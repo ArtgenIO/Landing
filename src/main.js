34c17d0b75d2cdf269ff54251c8c7ea2ce10b5f5
Varga Zsolt <hello@hisorange.me>
2022-01-20 11:27:09 +0100

    Small visual update before the rewamp

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import {
   AdditiveBlending,
   BufferAttribute,
   BufferGeometry,
-  Clock,
   Color,
   PerspectiveCamera,
   Points,
@@ -44,123 +43,93 @@ config.branches = 6;
 config.spin = -2;
 config.randomness = 8.5;
 config.randomnessPower = 4.9;
-config.stars = 18000;
-config.starColor = '#37393f';
-config.insideColor = '#2df0b2';
-config.outsideColor = '#4b21a8';
-
-let bgStarsGeometry = null;
-let bgStarsMaterial = null;
-let bgStars = null;
+config.stars = 1000;
+config.starColor = '#cccccc';
+config.insideColor = '#f55726';
+config.outsideColor = '#8100ff';
 
 //Background stars
-function generateBgStars() {
-  if (bgStars !== null) {
-    bgStarsGeometry.dispose();
-    bgStarsMaterial.dispose();
-    scene.remove(bgStars);
-  }
+const bgStarsGeometry = new BufferGeometry();
+const bgStarsPositions = new Float32Array(config.stars * 3);
 
-  bgStarsGeometry = new BufferGeometry();
-  const bgStarsPositions = new Float32Array(config.stars * 3);
-
-  for (let j = 0; j < config.stars; j++) {
-    bgStarsPositions[j * 3 + 0] = (Math.random() - 0.5) * 20;
-    bgStarsPositions[j * 3 + 1] = (Math.random() - 0.5) * 20;
-    bgStarsPositions[j * 3 + 2] = (Math.random() - 0.5) * 20;
-  }
-
-  bgStarsGeometry.setAttribute(
-    'position',
-    new BufferAttribute(bgStarsPositions, 3),
-  );
-
-  bgStarsMaterial = new PointsMaterial({
-    size: config.size,
-    depthWrite: false,
-    sizeAttenuation: true,
-    blending: AdditiveBlending,
-    color: config.starColor,
-    transparent: true,
-    alphaMap: shape,
-  });
-
-  bgStars = new Points(bgStarsGeometry, bgStarsMaterial);
-
-  scene.add(bgStars);
+for (let j = 0; j < config.stars; j++) {
+  bgStarsPositions[j * 3 + 0] = (Math.random() - 0.5) * 20;
+  bgStarsPositions[j * 3 + 1] = (Math.random() - 0.5) * 20;
+  bgStarsPositions[j * 3 + 2] = (Math.random() - 0.5) * 20;
 }
 
-generateBgStars();
+bgStarsGeometry.setAttribute(
+  'position',
+  new BufferAttribute(bgStarsPositions, 3),
+);
 
-// Generate the galaxy shape
-let geometry = null;
-let material = null;
-let points = null;
-let composer;
+bgStarsMaterial = new PointsMaterial({
+  size: config.size,
+  depthWrite: false,
+  sizeAttenuation: true,
+  blending: AdditiveBlending,
+  color: config.starColor,
+  transparent: true,
+  alphaMap: shape,
+});
 
-function generateGalaxy() {
-  if (points !== null) {
-    geometry.dispose();
-    material.dispose();
-    scene.remove(points);
-  }
+const bgStars = new Points(bgStarsGeometry, bgStarsMaterial);
 
-  geometry = new BufferGeometry();
+scene.add(bgStars);
 
-  const positions = new Float32Array(config.count * 3);
-  const colors = new Float32Array(config.count * 3);
+const geometry = new BufferGeometry();
 
-  const colorInside = new Color(config.insideColor);
-  const colorOutside = new Color(config.outsideColor);
+const positions = new Float32Array(config.count * 3);
+const colors = new Float32Array(config.count * 3);
 
-  for (let i = 0; i < config.count; i++) {
-    //Position
-    const x = Math.random() * config.radius;
-    const branchAngle = ((i % config.branches) / config.branches) * 2 * Math.PI;
-    const spinAngle = x * config.spin;
+const colorInside = new Color(config.insideColor);
+const colorOutside = new Color(config.outsideColor);
 
-    const randomX =
-      Math.pow(Math.random(), config.randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1);
-    const randomY =
-      Math.pow(Math.random(), config.randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1);
-    const randomZ =
-      Math.pow(Math.random(), config.randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1);
+for (let i = 0; i < config.count; i++) {
+  //Position
+  const x = Math.random() * config.radius;
+  const branchAngle = ((i % config.branches) / config.branches) * 2 * Math.PI;
+  const spinAngle = x * config.spin;
 
-    positions[i * 3] = Math.sin(branchAngle + spinAngle) * x + randomX;
-    positions[i * 3 + 1] = randomY;
-    positions[i * 3 + 2] = Math.cos(branchAngle + spinAngle) * x + randomZ;
+  const randomX =
+    Math.pow(Math.random(), config.randomnessPower) *
+    (Math.random() < 0.5 ? 1 : -1);
+  const randomY =
+    Math.pow(Math.random(), config.randomnessPower) *
+    (Math.random() < 0.5 ? 1 : -1);
+  const randomZ =
+    Math.pow(Math.random(), config.randomnessPower) *
+    (Math.random() < 0.5 ? 1 : -1);
 
-    // Color
-    const mixedColor = colorInside.clone();
-    mixedColor.lerp(colorOutside, x / config.radius);
+  positions[i * 3] = Math.sin(branchAngle + spinAngle) * x + randomX;
+  positions[i * 3 + 1] = randomY;
+  positions[i * 3 + 2] = Math.cos(branchAngle + spinAngle) * x + randomZ;
 
-    colors[i * 3 + 0] = mixedColor.r;
-    colors[i * 3 + 1] = mixedColor.g;
-    colors[i * 3 + 2] = mixedColor.b;
-  }
+  // Color
+  const mixedColor = colorInside.clone();
+  mixedColor.lerp(colorOutside, x / config.radius);
 
-  geometry.setAttribute('position', new BufferAttribute(positions, 3));
-  geometry.setAttribute('color', new BufferAttribute(colors, 3));
-
-  material = new PointsMaterial({
-    color: 'white',
-    size: config.size,
-    depthWrite: false,
-    sizeAttenuation: true,
-    blending: AdditiveBlending,
-    vertexColors: true,
-    transparent: true,
-    alphaMap: shape,
-  });
-
-  points = new Points(geometry, material);
-  scene.add(points);
+  colors[i * 3 + 0] = mixedColor.r;
+  colors[i * 3 + 1] = mixedColor.g;
+  colors[i * 3 + 2] = mixedColor.b;
 }
 
-generateGalaxy();
+geometry.setAttribute('position', new BufferAttribute(positions, 3));
+geometry.setAttribute('color', new BufferAttribute(colors, 3));
+
+const material = new PointsMaterial({
+  color: 'white',
+  size: config.size,
+  depthWrite: false,
+  sizeAttenuation: true,
+  blending: AdditiveBlending,
+  vertexColors: true,
+  transparent: true,
+  alphaMap: shape,
+});
+
+const points = new Points(geometry, material);
+scene.add(points);
 
 /**
  * Sizes
@@ -201,41 +170,46 @@ controls.enableDamping = true;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-const params = {
-  bloomStrength: 1.0,
-  bloomThreshold: 0.1,
-  bloomRadius: -0.25,
-};
-
 const renderScene = new RenderPass(scene, camera);
 
 const bloomPass = new UnrealBloomPass(
   new Vector2(window.innerWidth, window.innerHeight),
-  1.5,
+  8.5,
   0.4,
   0.85,
 );
-bloomPass.threshold = params.bloomThreshold;
-bloomPass.strength = params.bloomStrength;
-bloomPass.radius = params.bloomRadius;
+bloomPass.threshold = 0.05;
+bloomPass.strength = 1.2;
+bloomPass.radius = -0.25;
 
-composer = new EffectComposer(renderer);
+const composer = new EffectComposer(renderer);
 composer.addPass(renderScene);
 composer.addPass(bloomPass);
 
 // Animate
-const clock = new Clock();
+
+let frameCount = 0;
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+  frameCount++;
 
-  //Update the camera
-  points.rotation.y = -elapsedTime * 0.02;
-  bgStars.rotation.y = -elapsedTime * 0.002;
-  camera.rotation.x = 30;
+  // Rotate the stars
+  points.rotation.y += 0.0003;
+  bgStars.rotation.y -= 0.0018;
+
+  // Move around with the camera
+  const offsetX = (frameCount % (400 * 15)) / (80 * 15);
+  const moveX = (offsetX < 2.5 ? offsetX : 5 - offsetX) * 0.5;
+
+  const offsetY = (frameCount % (1600 * 7)) / (320 * 7);
+  const moveY = (offsetY < 2.5 ? offsetY : 5 - offsetY) * 1.5;
+
+  camera.position.x = 5 + moveX;
+  camera.rotation.x = 5 + moveX * 0.25;
+  camera.position.y = 2.5 + moveY * 0.5;
+  camera.rotation.y = 0.7 - moveY * 0.02;
 
   // Render
-  //renderer.render(scene, camera);
   composer.render();
 
   // Call tick again on the next frame
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,61 @@
-module.exports = {
+const { merge } = require('lodash');
+const { join } = require('path');
+const colors = require('tailwindcss/colors');
+
+const customColors = merge(colors, {
+  midnight: {
+    50: '#fefefe',
+    100: '#cfd2d9',
+    200: '#b5b9c2',
+    300: '#79808c',
+    400: '#5d616c',
+    500: '#474952',
+    600: '#37393f',
+    700: '#25272b',
+    750: '#202327',
+    800: '#15171b',
+    900: '#0a0d10',
+  },
+  primary: {
+    50: '#b2faff',
+    100: '#5df0fb',
+    200: '#52ebf6',
+    300: '#4fe1ec',
+    400: '#4bd5df',
+    500: '#46bdc6',
+    600: '#30868c',
+    700: '#1d5256',
+    800: '#102d30',
+    900: '#091819',
+  },
+});
+
+delete customColors['lightBlue'];
+delete customColors['warmGray'];
+delete customColors['trueGray'];
+delete customColors['coolGray'];
+delete customColors['blueGray'];
+
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig} */
+const config = {
   mode: 'jit',
   purge: {
-    content: ['./src/index.html'],
+    content: [join(__dirname, './src/**/*.{tsx,html}')],
     safelist: ['simple-lightbox'],
     safelistPatterns: [/^sl-/],
   },
-  darkMode: true, // or 'media' or 'class'
-  theme: {},
-  variants: {},
+  theme: {
+    fontFamily: {
+      header: ['Bebas Neue', 'cursive'],
+      code: ['Courier New', 'Courier', 'monospace'],
+      cursive: ['Gruppo', 'cursive'],
+    },
+    colors: customColors,
+  },
   plugins: [
     require('tailwindcss-textshadow'),
     require('tailwindcss-scrollbar'),
   ],
 };
+
+module.exports = config;
